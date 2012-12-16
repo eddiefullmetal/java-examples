703c2b5a2161abfb5f59fa6dbb32208e990e3ed2
Grigoriadis Grigoris <eddiefullmetal@gmail.com>
2012-12-16 19:46:08 +0200

    Added new products controller. Added jmeter with sample request. Added logback.xml config

---
+++ mongo-spring-data/src/main/java/grigoriadis/javaexamples/mongo/springdata/controller/ProductsController.java
@@ -0,0 +1,46 @@
+package grigoriadis.javaexamples.mongo.springdata.controller;
+
+import grigoriadis.javaexamples.mongo.springdata.model.Product;
+import grigoriadis.javaexamples.mongo.springdata.repository.ProductRepository;
+
+import org.apache.commons.lang3.StringUtils;
+import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/products")
+public class ProductsController
+{
+    private static final Logger logger = LoggerFactory.getLogger(ProductsController.class);
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Product getById(@PathVariable final String id)
+    {
+        if (StringUtils.isBlank(id))
+        {
+            logger.error("id cannot be null or empty");
+            throw new IllegalArgumentException("Id cannot be null or empty");
+        }
+
+        final Product product = this.productRepository.findOne(new ObjectId(id));
+
+        if (product == null)
+        {
+            logger.error("cannot find product with id: {}", id);
+            throw new IllegalArgumentException("invalid product id");
+        }
+
+        return product;
+    }
+}
