b0e5c5229a08ad04655c01da58a1bbbf8c59697b
Grigoriadis Grigoris <eddiefullmetal@gmail.com>
2012-12-16 03:04:21 +0200

    Added findByCategory

--- mongo-spring-data/src/main/java/grigoriadis/javaexamples/mongo/springdata/repository/ProductRepository.java
+++ mongo-spring-data/src/main/java/grigoriadis/javaexamples/mongo/springdata/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package grigoriadis.javaexamples.mongo.springdata.repository;
 
+import grigoriadis.javaexamples.mongo.springdata.model.Category;
 import grigoriadis.javaexamples.mongo.springdata.model.Product;
 
 import java.util.List;
@@ -11,6 +12,8 @@ import org.springframework.data.mongodb.repository.Query;
 
 public interface ProductRepository extends MongoRepository<Product, ObjectId>, ProductRepositoryCustom
 {
+    List<Product> findByCategory(Category category, Pageable pageable);
+
     List<Product> findByDescriptionLike(String description, Pageable pageable);
 
     Product findByName(String name);
